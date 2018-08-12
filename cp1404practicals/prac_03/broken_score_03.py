6c09ffaa56cc44df6b9d8ad24d98b68844a8cbdc
vickiedge <vicki.edge@my.jcu.edu.au>
2018-08-12 13:34:29 +0930

    Add function to calculate comment from score

--- cp1404practicals/prac_03/broken_score_03.py
+++ cp1404practicals/prac_03/broken_score_03.py
@@ -5,12 +5,20 @@ Broken program to determine score status
 
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-        print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    comment = calculate_comment_from_score(score)
+    print(comment)
+
+def calculate_comment_from_score(score):
+    if score < 0 or score > 100:
+        comment = "Invalid score"
+    elif score >= 90:
+        comment = "Excellent"
+    elif score >= 50:
+        comment = "Passable"
+    else:
+        comment = "Bad"
+    return comment
+
+main()
