4950072ce424457429ac48f4c726c43e09b0273c
vickiedge <vicki.edge@my.jcu.edu.au>
2018-08-12 13:26:00 +0930

    Add conversion calculation functions

--- cp1404practicals/prac_03/temperatures_03.py
+++ cp1404practicals/prac_03/temperatures_03.py
@@ -3,25 +3,33 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        # TODO: Write this section to convert F to C and display the result
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-        fahrenheit = float(input("Farenheit:  "))
-        celsius = 5 / 9 *(fahrenheit - 32)
-        print("Result: {:.2f} C".format(celsius))
-    else:
-        print("Invalid option")
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+            F - Convert Fahrenheit to Celsius
+            Q - Quit"""
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            fahrenheit = calculate_farenheit_from_celcius()
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            celsius = calculate_celsius_from_farenheit()
+            print("Result: {:.2f} C".format(celsius))
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+def calculate_farenheit_from_celcius():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+def calculate_celsius_from_farenheit():
+    fahrenheit = float(input("Farenheit:  "))
+    celsius = 5 / 9 *(fahrenheit - 32)
+    return celsius
+
+main()
